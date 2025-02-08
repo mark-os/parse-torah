5be19f76e3066f1fd90132a2be3ba36dc7d74cea
Mark <3282195+mark-os@users.noreply.github.com>
2025-02-08 02:00:10 -0600

    Remove import.py and export-old.py
    These files are no longer used, the current import2.py file is the
    new one, and it will be renamed to import.py

--- export-old.py
+++
@@ -1,8 +0,0 @@
-#sudo apt-get install libsword-utils
-from pysword.modules import SwordModules
-modules = SwordModules('WLC.zip')
-found_modules = modules.parse_modules()
-bible = modules.get_bible_from_module('WLC')
-output = bible.get(books=['micah'])
-with open('micah.txt','w') as file:
-    file.write(output)
